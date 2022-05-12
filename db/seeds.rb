b6a635954601302e5156449c837b24e428c0ed04
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-05-11 20:09:21 -0500

    fix 'password_digest' to 'password'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # creating owner info.
 
-julio = Owner.create(username: "Joker", email: "firstTest@gmail.com", password_digest: "testone")
-lunatwo = Owner.create(username: "LunaTwo", email: "LunaTwo@gmail.com", password_digest: "LunaTesttwo")
+julio = Owner.create(username: "Joker", email: "firstTest@gmail.com", password: "testone")
+lunatwo = Owner.create(username: "LunaTwo", email: "LunaTwo@gmail.com", password: "LunaTesttwo")
 Owner.create(username: "Test512", email: "512@test.com", password: "hello")
 # creating shoes info.
 
