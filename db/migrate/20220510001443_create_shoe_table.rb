f8c955c2314e2c33452c2dc20eb57a062af3719b
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-05-11 20:34:30 -0500

    restarted all my files

--- db/migrate/20220510001443_create_shoe_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateShoeTable < ActiveRecord::Migration[6.1]
-  def change
-    create_table :shoes do |t|
-      t.string :footware
-      t.integer :owner_id
-    end
-  end
-end
--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# creating owner info.
-
-julio = Owner.create(username: "Joker", email: "firstTest@gmail.com", password: "testone")
-lunatwo = Owner.create(username: "LunaTwo", email: "LunaTwo@gmail.com", password: "LunaTesttwo")
-Owner.create(username: "Test512", email: "512@test.com", password: "hello")
-# creating shoes info.
-
-Shoe.create(footware: "Nike Dunk Low Panda", owner_id: julio.id)
-Shoe.create(footware: "VaporMax Plus", owner_id: lunatwo.id)
