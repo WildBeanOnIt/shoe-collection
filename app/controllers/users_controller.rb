29ac1ab138ee7287c3ec03fe4673026838c195df
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-06-08 18:09:48 -0500

    auth. the user in the login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
     
     # shows the Sign Up form for new users.
     get "/signup" do
-        # if !logged_in?
+        if !logged_in?
             erb :"users/signup"
-        # else
-        #     redirect to "/index"
-        # end
+        else
+            redirect to "/index"
+        end
     end
 
     # post "/signup" do
@@ -14,17 +14,29 @@ class UsersController < ApplicationController
 
     #shows the login Form for excisting users.
     get "/login" do
-        erb :"users/login"
+        if !logged_in?
+            erb :"users/login"
+        else
+            redirect to "/index"
+        end
     end
 
     post "/login" do
-        @user = User.find_by(username: params[:username])
-        # auth. the user 
-        if @user.authenticate(params[:password])
-            # create a user session
-            binding.pry
+        # @user = User.find_by(username: params[:username])
+        # # auth. the user 
+        # if @user.authenticate(params[:password])
+        #     # create a user session
+        #     binding.pry
+        # else
+        #     # redirect them to signup form and tell them info is wrong
+        # end
+
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to "/index"
         else
-            # redirect them to signup form and tell them info is wrong
+            redirect to "/signup"
         end
     end
 end
